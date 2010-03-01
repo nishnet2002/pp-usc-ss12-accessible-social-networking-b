92385e6cc61bbc5dda245f6ce1e35d609af40e8a
vtambve <vtambve@a4c06626-2409-11df-9d2b-970631db4c00>
2010-03-01 00:57:07 +0000

    Final submission

---
+++ AxsTweetLoader.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name          Twitter Accessibility Enhancements
+// @namespace     
+// @description   AsxTweet
+// @include       http://*twitter.com/*
+// ==/UserScript==
+
+function loadScript(){
+  
+	var theLib = document.createElement('script');
+	theLib.type = 'text/javascript';
+	theLib.src = 'http://google-axsjax.googlecode.com/svn/trunk/common/AxsJAX.js';
+	var navLib = document.createElement('script');
+	navLib.type = 'text/javascript';
+	navLib.src = 'http://google-axsjax.googlecode.com/svn/trunk/common/AxsNav.js';
+	var lensLib = document.createElement('script');
+	lensLib.type = 'text/javascript';
+	lensLib.src = 'http://google-axsjax.googlecode.com/svn/trunk/common/AxsLens.js';
+	var sndLib = document.createElement('script');
+	sndLib.type = 'text/javascript';
+	sndLib.src = 'http://google-axsjax.googlecode.com/svn/trunk/common/AxsSound.js';
+	var pkLib = document.createElement('script');
+	pkLib.type = 'text/javascript';
+	pkLib.src = 'http://google-axsjax.googlecode.com/svn/trunk/common/PowerKey.js';
+
+	document.getElementsByTagName('head')[0].appendChild(theLib);
+	document.getElementsByTagName('head')[0].appendChild(navLib);
+	document.getElementsByTagName('head')[0].appendChild(lensLib);
+	document.getElementsByTagName('head')[0].appendChild(sndLib);
+	document.getElementsByTagName('head')[0].appendChild(pkLib);
+  
+  var theScript = document.createElement('script');
+  theScript.type = 'text/javascript';
+  theScript.src = 'http://localhost:4001/AxsTweet.js';
+  document.getElementsByTagName('head')[0].appendChild(theScript);
+}
+
+loadScript();
